885dfc65e11fa9edcfdf1e628033d5a64a559ea3
Lizari <muroya725@gmail.com>
2022-05-14 22:43:48 +0900

    取り敢えず一時的に各ページを追加

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import Header from '@/components/Header';
+
+const About: NextPage = () => {
+  return(
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default About

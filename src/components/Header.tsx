71d23e46cf9157f6fc7f55f66cc5b8dc47e913fa
Lizari <muroya725@gmail.com>
2022-05-16 10:37:10 +0900

    Headerをhoverしたとき色変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,28 +3,43 @@ import { Link, Stack } from '@mui/material';
 
 const Header: React.VFC = () => {
   return (
-    <Stack direction={'row'} spacing={3} justifyContent={'center'}>
+    <Stack mt={3} direction={'row'} spacing={3} justifyContent={'center'}>
       <Link
         href={'/'}
-        color={'#FFFFFF'}
         fontSize={'x-large'}
         underline={'hover'}
+        sx={{
+          color: 'gray',
+          '&:hover': {
+            color: '#FFFFFF',
+          },
+        }}
       >
         LRC Player
       </Link>
       <Link
         href={'/about'}
-        color={'#FFFFFF'}
         fontSize={'x-large'}
         underline={'hover'}
+        sx={{
+          color: 'gray',
+          '&:hover': {
+            color: '#FFFFFF',
+          },
+        }}
       >
         About
       </Link>
       <Link
         href={'/usage'}
-        color={'#FFFFFF'}
         fontSize={'x-large'}
         underline={'hover'}
+        sx={{
+          color: 'gray',
+          '&:hover': {
+            color: '#FFFFFF',
+          },
+        }}
       >
         Usage
       </Link>

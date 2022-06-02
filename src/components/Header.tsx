8f1f1b7759500f01d0211713e4ddacadaddc9809
Lizari <muroya725@gmail.com>
2022-06-03 02:29:11 +0900

    Headerをハンバーガーメニュー方式に変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,65 @@
-import React from 'react';
-import { Link, Stack } from '@mui/material';
+import React, { useState } from 'react';
+import { Drawer, IconButton, Link, Stack } from '@mui/material';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
 const Header: React.VFC = () => {
+  const [isOpen, toggleDrawer] = useState(false);
+
   return (
-    <Stack mt={3} direction={'row'} spacing={3} justifyContent={'center'}>
-      <Link
-        href={'/'}
-        fontSize={'x-large'}
-        fontWeight={"700"}
-        underline={'none'}
+    <div>
+      <IconButton
         sx={{
-          color: '#808080',
-          '&:hover': {
-            color: '#FFFFFF',
+          paddingTop: 3,
+          paddingLeft: 3,
+        }}
+        onClick={() => toggleDrawer(true)}
+      >
+        <MenuOutlinedIcon fontSize={'large'} sx={{ color: 'white' }} />
+      </IconButton>
+      <Drawer
+        anchor={'left'}
+        open={isOpen}
+        onClose={() => toggleDrawer(false)}
+        PaperProps={{
+          sx: {
+            backgroundColor: '#181818',
+            paddingTop: 3,
+            width: '20%',
           },
         }}
       >
-        LRC Player
-      </Link>
-      <Link
-        href={'/about'}
-        fontSize={'x-large'}
-        fontWeight={"700"}
-        underline={'none'}
-        sx={{
-          color: '#808080',
-          '&:hover': {
-            color: '#FFFFFF',
-          },
-        }}
-      >
-        About
-      </Link>
-    </Stack>
+        <Stack textAlign={'center'} spacing={5}>
+          <Link
+            href={'/'}
+            fontSize={'x-large'}
+            fontWeight={'700'}
+            underline={'none'}
+            sx={{
+              color: '#808080',
+              '&:hover': {
+                color: '#FFFFFF',
+              },
+            }}
+          >
+            LRC Player
+          </Link>
+          <Link
+            href={'/about'}
+            fontSize={'large'}
+            fontWeight={'700'}
+            underline={'none'}
+            sx={{
+              color: '#808080',
+              '&:hover': {
+                color: '#FFFFFF',
+              },
+            }}
+          >
+            About
+          </Link>
+        </Stack>
+      </Drawer>
+    </div>
   );
 };
 
